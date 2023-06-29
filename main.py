606368664346257779925496e2ea78bd2eb08edc
vimrc <vyciouz@gmail.com>
2023-06-29 14:05:53 -0600

    Ligero ajuste para que pueda correr en replit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from website import create_app
 # from db import *
 
 
-# connectoToDB()
+
 app  = create_app()
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='8080', debug=True) #Ajuste para correr enn replit, que ocupa ue sea host 0.0.0.0
+    # app.run(debug=True)
 
 # @app.route('/')
 # def home():
